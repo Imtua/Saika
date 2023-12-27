b34a8f7f2922e292e24fd16d94b0110590b8fcad
Imtua <itsololevelling@gmail.com>
2023-12-28 02:05:16 +0300

    Add project files.

---
+++ Saika.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Saika.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
